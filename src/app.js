be591c5dd21e4b310e2238cfbac0e1aeeb35fb3b
Oleg <oleg.korolenko@gmail.com>
2016-08-28 19:51:08 +0200

    basic experss config

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
 const apiKey = process.env.TELEGRAM_API_KEY;
 const telegram = require('telegram-node-bot');
 const bot = new telegram.Telegram(apiKey);
